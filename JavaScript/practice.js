e6e553f61ebdba5eb89579c0752acd080b237356
Eswar <eeswar117@gmail.com>
2023-08-14 23:13:28 +0530

    practicing let

---
+++ JavaScript/practice.js
@@ -0,0 +1,12 @@
+let name = "Arudra Eswar";
+console.log(name);
+
+let x = 5 + 6 + "10";
+console.log(x)
+
+let y = "10" + 8 + 7;
+console.log(y)
+
+let l = "Enukonda" + " " + "Khanna";
+console.log(l)
+
