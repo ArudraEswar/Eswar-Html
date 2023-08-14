e9e3798699744aad408781da36e3855948f02f2d
Eswar <eeswar117@gmail.com>
2023-08-14 22:19:26 +0530

    JavaScript Console

---
+++ JavaScript/basic.js
@@ -0,0 +1,26 @@
+// console.log('Eswar');
+let x = 10;
+
+let y = 15;
+
+console.log(x+y);
+
+let l= "15";
+
+let m= 25;
+
+console.log(l+m);
+
+let name= "My Name is Eswar";
+console.log(name);
+
+let name1= "My Name is 'Khanna'";
+console.log(name1);
+
+let name2= "'Our Project is going good and Java Script Is Nice'";
+console.log(name2);
+
+let j=10;
+
+let name3= "Lavanya";
+console.log(`${name3} has ${j} rupees`);
