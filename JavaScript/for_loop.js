afc241146e2e946eea4b329c77f4c327003e53e8
Eswar <eeswar117@gmail.com>
2023-08-25 15:28:57 +0530

    for loop

---
+++ JavaScript/for_loop.js
@@ -0,0 +1,12 @@
+let data = "";
+for (let i = 0; i < 5; i++) {
+    data += "The Number Starts From" + "  "+i ;
+}
+console.log(data);
+
+let number = [];
+for (let j = 0; j <9; j++) {
+    number = number +"  "+ j;
+}
+console.log(number);
+
