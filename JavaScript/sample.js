d70ed4714bd58bcb4e6f6b065528ce4716bbf646
Eswar <eeswar117@gmail.com>
2023-09-05 21:03:31 +0530

    splice

---
+++ JavaScript/sample.js
@@ -0,0 +1,9 @@
+const fruits =["Apple", "Banana", "Orange", "Mango"];
+const data = fruits.splice(2,2);
+console.log(data);
+data.push("Lemon", "Beetroot");
+console.log(data);
+data.pop();
+console.log(data);
+const heig = data.slice(0);
+console.log(heig);
