3009ac4c57d0f725dd878eab0c7cdc56862559fe
ArudraEswar <eeswar117@gmail.com>
2023-12-27 19:52:58 +0530

    changed js file

--- JavaScript/Function/scrpit.js
+++ JavaScript/Function/scrpit.js
@@ -1,8 +1,4 @@
-// function greet(name) {
-//     console.log("Hello, " + name + "!");
-//   }
-  
-//   greet("Eswar");
+
 
 document.getElementById("myButton").addEventListener("click", function() {
     alert("Button was clicked!");
