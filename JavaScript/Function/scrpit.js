deb2e8ff8ec859e23195f3094e58f06ed4eb5d66
ArudraEswar <eeswar117@gmail.com>
2023-12-27 19:52:24 +0530

    Click Event

---
+++ JavaScript/Function/scrpit.js
@@ -0,0 +1,10 @@
+// function greet(name) {
+//     console.log("Hello, " + name + "!");
+//   }
+  
+//   greet("Eswar");
+
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+  });
+  
