bd802ea71c4b95d6340a540bde3689144e13b819
ArudraEswar <eeswar117@gmail.com>
2023-12-28 20:09:18 +0530

    Positive or Negative Number

---
+++ JavaScript/Java Script/Positive Or Negative/script.js
@@ -0,0 +1,15 @@
+function checkNumber(number) {
+    if (number > 0) {
+      console.log("Positive number");
+    } else if (number < 0) {
+      console.log("Negative number");
+    } else {
+      console.log("Zero");
+    }
+  }
+  
+  checkNumber(5); 
+  checkNumber(-3); 
+  checkNumber(0); 
+  checkNumber("4");
+  
